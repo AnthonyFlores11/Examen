f3186dcd4405c276b18f664b3f72d8a0c8d94a02
ANTHONY FLORES AGUERO <anthony.flores1@uamcr.net>
2020-02-25 21:30:01 -0600

    Examen 1

---
+++ Acceso a datos/IOperaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.Acceso_a_datos
+{
+   public interface IOperaciones<T>
+    {
+        void Insertar(T item);
+        T Buscar(int id);
+        List<T> BuscarTodo();
+        void Modificar(T item);
+        void Eliminar(T item);
+    }
+}
---
+++ Acceso a datos/OperacionesPersona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Acceso_a_datos;
+using System.Data.SqlClient;
+
+namespace Examen.Acceso_a_datos
+{
+     
+    public class OperacionesPersona : IOperaciones<Persona>
+    {
+        private ConeccionBD coneccion = new ConeccionBD();
+        public Persona Buscar(int id)
+        {
+            string query = string.Format("Select * FROM Persona WHERE id={0}", id);
+            SqlDataReader resultado = coneccion.ConexionSQLQuery(query);
+            if(resultado != null)
+            {
+                while (resultado.NextResult())
+                {
+                    return new Persona(resultado.GetInt32(0), resultado.GetString(1), resultado.GetString(2));
+                }
+            }
+            return null;
+            
+        }
+
+        public List<Persona> BuscarTodo()
+        {
+            string query = "SELECT * FROM Persona";
+            List<Persona> persona = new List<Persona>();
+            SqlDataReader resultado = coneccion.ConexionSQLQuery(query);
+            if (resultado != null)
+            {
+                while (resultado.NextResult())
+                {
+                    persona.Add(new Persona (resultado.GetInt32(0), resultado.GetString(1), resultado.GetString(1)));
+                }
+            }
+            return null;
+
+        }
+
+        public void Eliminar(Persona item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insertar(Persona item)
+        {
+            string query = "INSERT INTO dbo.Persona VALUES("+ item.Id +" , "+ item.Nombre + ","+ item.Email +")";
+            SqlDataReader resultado = coneccion.ConexionSQLQuery(query);
+        }
+
+        public void Modificar(Persona item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
